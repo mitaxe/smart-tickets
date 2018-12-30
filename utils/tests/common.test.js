c72c5ad81704910bb1fa117da928f8633ba12a59
BogdanNosovytskyy <b.nosovytskyy@gmail.com>
2018-12-30 21:32:37 +0200

    ci-check

--- utils/tests/common.test.js
+++ utils/tests/common.test.js
@@ -2,6 +2,6 @@ import { sum } from 'utils/common'
 
 describe('test example', () => {
   it('should return correct sum', () => {
-    expect(sum(1, 2)).toEqual(3)
+    expect(sum(1, 2)).toEqual(2)
   })
 })
